dde0191e2fcfd8e1165b049dbadb00553109ca54
sohanwithanm <sohamacharya7@gmail.com>
2026-02-24 05:05:27 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json()); // to recieve and send data in jsons (middleware)
 // to allow frontend-backend communication
 app.use(cors({
-  origin: ["http://localhost:5173", "https://your-vercel-link.vercel.app"],
+  origin: ["http://localhost:5173", "https://felicity-event-management-3sve1013l-sohanwithanms-projects.vercel.app"],
   credentials: true
 }));
 
